7e4db3d74c9248df9f8175921ff203855a3fb665
Matthias Kestenholz <mk@feinheit.ch>
2018-09-27 08:58:34 +0200

    Re-generate migrations

--- specifications/migrations/0001_initial.py
+++ specifications/migrations/0001_initial.py
@@ -1,80 +1,68 @@
 # -*- coding: utf-8 -*-
-import datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import django.db.models.deletion
 
 
-class Migration(SchemaMigration):
+class Migration(migrations.Migration):
 
-    def forwards(self, orm):
-        # Adding model 'Specification'
-        db.create_table('specifications_specification', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('name', self.gf('django.db.models.fields.CharField')(max_length=100)),
-            ('notes', self.gf('django.db.models.fields.TextField')(blank=True)),
-        ))
-        db.send_create_signal('specifications', ['Specification'])
+    dependencies = [
+    ]
 
-        # Adding model 'SpecificationFieldGroup'
-        db.create_table('specifications_specificationfieldgroup', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('specification', self.gf('django.db.models.fields.related.ForeignKey')(related_name='groups', to=orm['specifications.Specification'])),
-            ('name', self.gf('django.db.models.fields.CharField')(max_length=100)),
-            ('ordering', self.gf('django.db.models.fields.IntegerField')(default=0)),
-        ))
-        db.send_create_signal('specifications', ['SpecificationFieldGroup'])
-
-        # Adding model 'SpecificationField'
-        db.create_table('specifications_specificationfield', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('name', self.gf('django.db.models.fields.CharField')(max_length=100)),
-            ('type', self.gf('django.db.models.fields.CharField')(max_length=20)),
-            ('choices', self.gf('django.db.models.fields.TextField')(blank=True)),
-            ('help_text', self.gf('django.db.models.fields.CharField')(default='', max_length=100, blank=True)),
-            ('required', self.gf('django.db.models.fields.BooleanField')(default=False)),
-            ('ordering', self.gf('django.db.models.fields.IntegerField')(default=0)),
-            ('specification', self.gf('django.db.models.fields.related.ForeignKey')(related_name='fields', to=orm['specifications.Specification'])),
-            ('group', self.gf('django.db.models.fields.related.ForeignKey')(blank=True, related_name='fields', null=True, to=orm['specifications.SpecificationFieldGroup'])),
-        ))
-        db.send_create_signal('specifications', ['SpecificationField'])
-
-    def backwards(self, orm):
-        # Deleting model 'Specification'
-        db.delete_table('specifications_specification')
-
-        # Deleting model 'SpecificationFieldGroup'
-        db.delete_table('specifications_specificationfieldgroup')
-
-        # Deleting model 'SpecificationField'
-        db.delete_table('specifications_specificationfield')
-
-    models = {
-        'specifications.specification': {
-            'Meta': {'ordering': "['name']", 'object_name': 'Specification'},
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'notes': ('django.db.models.fields.TextField', [], {'blank': 'True'})
-        },
-        'specifications.specificationfield': {
-            'Meta': {'ordering': "['group__ordering', 'ordering']", 'object_name': 'SpecificationField'},
-            'choices': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
-            'group': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'fields'", 'null': 'True', 'to': "orm['specifications.SpecificationFieldGroup']"}),
-            'help_text': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '100', 'blank': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'ordering': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
-            'required': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'specification': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'fields'", 'to': "orm['specifications.Specification']"}),
-            'type': ('django.db.models.fields.CharField', [], {'max_length': '20'})
-        },
-        'specifications.specificationfieldgroup': {
-            'Meta': {'ordering': "['ordering']", 'object_name': 'SpecificationFieldGroup'},
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'ordering': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
-            'specification': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'groups'", 'to': "orm['specifications.Specification']"})
-        }
-    }
-
-    complete_apps = ['specifications']
+    operations = [
+        migrations.CreateModel(
+            name='Specification',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, verbose_name='ID', serialize=False)),
+                ('name', models.CharField(verbose_name='name', max_length=100)),
+                ('notes', models.TextField(blank=True, verbose_name='notes')),
+            ],
+            options={
+                'verbose_name_plural': 'specifications',
+                'verbose_name': 'specification',
+                'ordering': ['name'],
+            },
+        ),
+        migrations.CreateModel(
+            name='SpecificationField',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, verbose_name='ID', serialize=False)),
+                ('name', models.CharField(verbose_name='name', max_length=100)),
+                ('type', models.CharField(choices=[(b'text', 'text'), (b'longtext', 'long text'), (b'boolean', 'boolean'), (b'integer', 'integer'), (b'closed_set_single', 'closed set'), (b'closed_set_multiple', 'closed set (multiple)'), (b'open_set_single', 'open set'), (b'open_set_single_extensible', b'open set (extensible)')], verbose_name='type', max_length=30)),
+                ('choices', models.TextField(blank=True, verbose_name='choices', help_text='One choice per line (if applicable).')),
+                ('help_text', models.CharField(blank=True, verbose_name='help text', max_length=100, default=b'')),
+                ('required', models.BooleanField(verbose_name='required')),
+                ('ordering', models.IntegerField(verbose_name='ordering', default=0)),
+            ],
+            options={
+                'verbose_name_plural': 'specification fields',
+                'verbose_name': 'specification field',
+                'ordering': ['group__ordering', 'ordering'],
+            },
+        ),
+        migrations.CreateModel(
+            name='SpecificationFieldGroup',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, verbose_name='ID', serialize=False)),
+                ('name', models.CharField(verbose_name='name', max_length=100)),
+                ('ordering', models.IntegerField(verbose_name='ordering', default=0)),
+                ('specification', models.ForeignKey(to='specifications.Specification', related_name='groups', verbose_name='specification')),
+            ],
+            options={
+                'verbose_name_plural': 'specification field groups',
+                'verbose_name': 'specification field group',
+                'ordering': ['ordering'],
+            },
+        ),
+        migrations.AddField(
+            model_name='specificationfield',
+            name='group',
+            field=models.ForeignKey(on_delete=django.db.models.deletion.SET_NULL, to='specifications.SpecificationFieldGroup', blank=True, related_name='fields', verbose_name='group', null=True),
+        ),
+        migrations.AddField(
+            model_name='specificationfield',
+            name='specification',
+            field=models.ForeignKey(to='specifications.Specification', related_name='fields', verbose_name='specification'),
+        ),
+    ]
